599ca51aee0f86d62bd93f615cc1f9c591922ec7
artem <yl4-bot>
2022-11-06 10:38:42 +0300

    main kod

--- project pqt5/main kod.py
+++ project pqt5/main kod.py
@@ -1,41 +1,47 @@
 import sys
 
+from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from start import Ui_MainWindowStart
 from home import Ui_MainWindowHome
 from info import Ui_MainWindowInfo
 import imges
+import datetime
+
+
+class WrongDate(Exception):
+    pass
+
+
+class WrongWage(Exception):
+    pass
 
 
 class InfoW(QMainWindow, Ui_MainWindowInfo):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.start_perehod)
 
-        self.pushButton.clicked.connect(self.run)
-
-    def run(self):
+    def start_perehod(self):
         self.window = StartW()
         self.window.show()
         self.close()
-
-
 
 
 class StartW(QMainWindow, Ui_MainWindowStart):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.home_perehod)
+        self.pushButton_2.clicked.connect(self.info_perehod)
 
-        self.pushButton.clicked.connect(self.run)
-        self.pushButton_2.clicked.connect(self.run1)
-
-    def run(self):
+    def home_perehod(self):
         self.window = HomeW()
         self.window.show()
         self.close()
 
-    def run1(self):
+    def info_perehod(self):
         self.window = InfoW()
         self.window.show()
         self.close()
@@ -45,13 +51,42 @@ class HomeW(QMainWindow, Ui_MainWindowHome):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.pushButton_4.clicked.connect(self.move_back)
+        self.pushButton_5.clicked.connect(self.user_inf)
 
-        self.pushButton_4.clicked.connect(self.run)
-
-    def run(self):
+    def move_back(self):
         self.window = StartW()
         self.window.show()
         self.close()
+
+    def sbor(self):
+        spis = []
+        if self.dateEdit.date() <= QDate.currentDate():
+            raise WrongDate
+        else:
+            spis.append(self.dateEdit.date())
+        if int(self.lineEdit.text()) <= 0:
+            raise WrongWage
+        else:
+            spis.append(self.lineEdit.text())
+        if len(spis) == 2:
+            return spis
+
+
+    def user_inf(self):
+        try:
+            a = self.sbor()
+            data = a[0]
+            zarplata = a[1]
+        except WrongDate:
+            self.label_15.setText('Робот не доволен, ошибка в дате')
+        except WrongWage:
+            self.label_15.setText('Робот не доволен, ошибка в деньгах')
+        except ValueError:
+            self.label_15.setText('Робот не доволен, ошибка в вводе')
+
+
+
 
 
 if __name__ == '__main__':
