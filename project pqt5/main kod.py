039d0b043c966a58ecdee4927ace16708fd73234
artem <yl4-bot>
2022-11-22 20:30:42 +0300

    main kod

--- project pqt5/main kod.py
+++ project pqt5/main kod.py
@@ -1,14 +1,18 @@
 import sys
 import sqlite3
 from PyQt5.QtCore import QDate
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QLabel
 
 from start import Ui_MainWindowStart
 from home import Ui_MainWindowHome
 from info import Ui_MainWindowInfo
 import changing_bd
 import view_of_bd
+import choise
+import choise2
 import imges
+
+GLOBAL = None
 
 
 class WrongDate(Exception):
@@ -18,6 +22,87 @@ class WrongDate(Exception):
 class WrongWage(Exception):
     pass
 
+
+class ChoisePokW(QMainWindow, choise2.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.comboBox.activated.connect(self.handleActivated)
+        self.znach = 'id'
+
+        self.pushButton.clicked.connect(self.run)
+
+    def handleActivated(self, text):
+        self.znach = ['id', 'название', 'цена', 'магазин'][int(text)]
+
+    def run(self):
+        text = self.lineEdit.text()
+        try:
+            buy = []
+            con = sqlite3.connect("basa.db")
+            cur = con.cursor()
+            if self.znach == 'id':
+                buy = cur.execute("""SELECT название, цена, магазин FROM buy WHERE id = ?""", (text,)).fetchall()
+            if self.znach == 'цена':
+                buy = cur.execute("""SELECT название, цена, магазин FROM buy WHERE цена = ?""", (text,)).fetchall()
+            if self.znach == 'магазин':
+                buy = cur.execute("""SELECT название, цена, магазин FROM buy WHERE магазин = ?""", (text,)).fetchall()
+            if self.znach == 'название':
+                buy = cur.execute("""SELECT название, цена, магазин FROM buy WHERE название = ?""", (text,)).fetchall()
+            cur.close()
+            con.close()
+
+            if len(buy) == 0:
+                raise ValueError
+            else:
+                ViewW.make_tabl_pok(GLOBAL, buy)
+                self.close()
+        except ValueError:
+            self.lineEdit.setText("НЕТ ТАКОГО")
+        except Exception:
+            self.lineEdit.setText("ОШИБКА В ДАННЫХ, TRY AGAIN")
+
+
+class ChoiseWantW(QMainWindow, choise.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.comboBox.activated.connect(self.handleActivated)
+        self.znach = 'id'
+
+        self.pushButton.clicked.connect(self.run)
+
+    def handleActivated(self, text):
+        self.znach = ['id', 'название', 'цена', 'магазин'][int(text)]
+
+    def run(self):
+        text = self.lineEdit.text()
+        try:
+            buy = []
+            con = sqlite3.connect("basa.db")
+            cur = con.cursor()
+            if self.znach == 'id':
+                buy = cur.execute("""SELECT название, цена, магазин FROM want WHERE id = ?""", (text,)).fetchall()
+            if self.znach == 'цена':
+                buy = cur.execute("""SELECT название, цена, магазин FROM want WHERE цена = ?""", (text,)).fetchall()
+            if self.znach == 'магазин':
+                buy = cur.execute("""SELECT название, цена, магазин FROM want WHERE магазин = ?""", (text,)).fetchall()
+            if self.znach == 'название':
+                buy = cur.execute("""SELECT название, цена, магазин FROM want WHERE название = ?""", (text,)).fetchall()
+            cur.close()
+            con.close()
+
+            if len(buy) == 0:
+                raise ValueError
+            else:
+                ViewW.make_tabl_want(GLOBAL, buy)
+                self.close()
+        except ValueError:
+            self.lineEdit.setText("НЕТ ТАКОГО")
+        except Exception:
+            self.lineEdit.setText("ОШИБКА В ДАННЫХ, TRY AGAIN")
 
 class InfoW(QMainWindow, Ui_MainWindowInfo):
     def __init__(self):
@@ -49,7 +134,7 @@ class StartW(QMainWindow, Ui_MainWindowStart):
         self.close()
 
 
-class ChangingW(QMainWindow, changing_bd.Ui_MainWindow):
+class ChangingW(QMainWindow, changing_bd.Ui_MainWindow): # сделать
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -57,8 +142,100 @@ class ChangingW(QMainWindow, changing_bd.Ui_MainWindow):
 
 class ViewW(QMainWindow, view_of_bd.Ui_MainWindow):
     def __init__(self):
+        global GLOBAL
         super().__init__()
         self.setupUi(self)
+
+        GLOBAL = self
+
+        self.making_table()
+
+        self.pushButton_4.clicked.connect(self.move_back)
+        self.pushButton_2.clicked.connect(self.changing_bd_win)
+
+        self.pushButton_5.clicked.connect(self.poisk_v_want)
+        self.pushButton.clicked.connect(self.poisk_v_pok)
+
+
+    def make_tabl_pok(self, res):
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(['название', 'цена', 'магазин'])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def make_tabl_want(self, res1):
+        self.tableWidget_2.setColumnCount(3)
+        self.tableWidget_2.setRowCount(0)
+        self.tableWidget_2.setHorizontalHeaderLabels(['название', 'цена', 'магазин'])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res1):
+            self.tableWidget_2.setRowCount(
+                self.tableWidget_2.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget_2.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+
+    def poisk_v_pok(self):
+        self.window = ChoisePokW()
+        self.window.show()
+
+    def poisk_v_want(self):
+        self.window = ChoiseWantW()
+        self.window.show()
+
+
+    def move_back(self):
+        self.window = HomeW()
+        self.window.show()
+        self.close()
+
+    def changing_bd_win(self):
+        self.window = ChangingW()
+        self.window.show()
+        self.close()
+
+    def making_table(self):
+        #добавление в таблицы информации из бд о покупках
+        con = sqlite3.connect("basa.db")
+        cur = con.cursor()
+
+        buy = cur.execute("""SELECT * FROM buy""").fetchall()
+        res = [i[1:] for i in buy]
+
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(['название', 'цена', 'магазин'])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+        last = cur.execute("""SELECT * FROM want""").fetchall()
+        res1 = [i[1:] for i in last]
+
+        self.tableWidget_2.setColumnCount(3)
+        self.tableWidget_2.setRowCount(0)
+        self.tableWidget_2.setHorizontalHeaderLabels(['название', 'цена', 'магазин'])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res1):
+            self.tableWidget_2.setRowCount(
+                self.tableWidget_2.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget_2.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+        cur.close()
+        con.close()
 
 
 class HomeW(QMainWindow, Ui_MainWindowHome):
@@ -95,9 +272,6 @@ class HomeW(QMainWindow, Ui_MainWindowHome):
         cur = con.cursor()
         result = cur.execute("""SELECT * FROM user""").fetchall()[0]
 
-        cur.close()
-        con.close()
-
         if len(str(result[4])) == 1:
             a = f'0{result[4]}'
         else:
@@ -112,9 +286,39 @@ class HomeW(QMainWindow, Ui_MainWindowHome):
         a = QDate.currentDate().toString('yyyy-MM-dd')
         now = QDate.fromString(a, "yyyy-MM-dd")
 
-
         #добавление в таблицы информации из бд о покупках
 
+        buy = cur.execute("""SELECT * FROM buy""").fetchall()
+        buy.sort(key=lambda x: x[2], reverse=True)
+        res = [i[1:] for i in buy]
+
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(['название', 'цена', 'магазин'])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+        last = cur.execute("""SELECT * FROM buy""").fetchall()[::-1]
+        res1 = [i[1:] for i in last]
+
+        self.tableWidget_2.setColumnCount(3)
+        self.tableWidget_2.setRowCount(0)
+        self.tableWidget_2.setHorizontalHeaderLabels(['название', 'цена', 'магазин'])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res1):
+            self.tableWidget_2.setRowCount(
+                self.tableWidget_2.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget_2.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+        cur.close()
+        con.close()
 
         try:
             if firstDay == now:
@@ -127,7 +331,6 @@ class HomeW(QMainWindow, Ui_MainWindowHome):
                 cur = con.cursor()
                 cur.execute(f"""UPDATE user SET потрачено = {0}""")
             else:
-
                 numDay = now.daysTo(firstDay)
                 self.label_11.setText(str(numDay))
 
@@ -194,17 +397,12 @@ class HomeW(QMainWindow, Ui_MainWindowHome):
             con.close()
 
             self.start_info()
-
         except WrongDate:
             self.label_15.setText('Робот не доволен, ошибка в дате')
         except WrongWage:
             self.label_15.setText('Робот не доволен, ошибка в деньгах')
         except ValueError:
             self.label_15.setText('Робот не доволен, ошибка в вводе')
-
-
-
-
 
 
 
---
+++ project pqt5/test kode.py
@@ -0,0 +1,5 @@
+res = []
+for i in range(len(buy)):
+    if i != 0:
+        res.append(buy[i])
+res = (res[0], res[1], res[2])
